75f76ee9c38cc0506f1f266c003b01be57c5e120
Archy-Official <archy.discord.bots@atomicmail.io>
2026-06-04 22:35:07 +0500

    Create guildCreate.js

---
+++ Discloud/src/events/guildCreate.js
@@ -0,0 +1,3 @@
+export default function handleGuildCreate(guild, guildConfigCache) {
+  console.log(`[Bot Action Alert] Gateway Bot successfully introduced to a new client instance: ${guild.name} (ID: ${guild.id})`);
+}
